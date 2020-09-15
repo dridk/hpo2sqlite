456e4e3ddba924c9054883e93dfeed39841f8a00
Sacha Schutz <sschutz@Lanascol>
2020-09-15 18:53:12 +0200

    new import gene

--- import_gene.py
+++ import_gene.py
@@ -4,67 +4,66 @@ from collections import defaultdict
 import os
 from model import *
 import sys
-from peewee import * 
+from peewee import *
 
-# Gene to phenotypes 
-# http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt
-gene_file = str(sys.argv[1]) 
+#  Gene to phenotypes
+#  http://compbio.charite.de/jenkins/job/hpo.annotations.monthly/lastSuccessfulBuild/artifact/annotation/ALL_SOURCES_ALL_FREQUENCIES_genes_to_phenotype.txt
+gene_file = str(sys.argv[1])
 
 
 db = SqliteDatabase("hpo.db")
 
 try:
-	db.create_tables([Genes,Term_has_Genes])
+    db.create_tables([Genes, Term_has_Genes])
 except:
-	pass
+    pass
 
 
 # save all hpo in memory map
 hpo = {}
 for term in Terms.select():
-	hpo[term.hpo] = term
+    hpo[term.hpo] = term
 
 
-# Load all genes 
+# Load all genes
 genes = set()
-
+print("create genes tables")
 with open(gene_file, "r") as file:
-	next(file)
-	for line in file:
-		row  = line.rstrip().split('\t')
+    next(file)
+    for line in file:
+        row = line.rstrip().split("\t")
+        # entrezid / gene symbole
+        genes.add((int(row[2]), row[3]))
 
-		genes.add((int(row[0]), row[1]))
 
+data_source = [{"entrez_id": i[0], "name": i[1]} for i in genes]
 
-data_source = [ {"entrez_id": i[0], "name":i[1]}for i in genes]
-
-print("save gene")
 with db.atomic():
-	for data_dict in data_source:
-		Genes.create(**data_dict)
+    Genes.insert_many(data_source)
+
+    # for data_dict in data_source:
+    #    Genes.create(**data_dict)
 
 
 # save all hpo in memory map
 genes = {}
 for gene in Genes.select():
-	genes[gene.name] = gene
+    genes[gene.name] = gene
 
 
-# Save hpo_has_gene 
+# Save hpo_has_gene
 hpo_genes = []
-print("save hpo has gene")
+print("create hpo_has_gene tables")
 
 with open(gene_file, "r") as file:
-	next(file)
-	for line in file:
-		row  = line.rstrip().split('\t')
+    next(file)
+    for line in file:
+        row = line.rstrip().split("\t")
+        gene = genes[row[3]]
+        term = hpo[row[0]]
 
-		gene    = genes[row[1]]
-		term    = hpo[row[3]]
-
-		hpo_genes.append({"term":term, "gene":gene})
+        hpo_genes.append({"term": term.id, "gene": gene.id})
 
 
 with db.atomic():
-    for item in hpo_genes:
-        Term_has_Genes.create(**item)
+    Genes_has_Terms.insert_many(hpo_genes)
--- model.py
+++ model.py
@@ -1,71 +1,78 @@
-from peewee import * 
-import os 
+from peewee import *
+import os
 
-# Create Sqlite model 
+# Create Sqlite model
 
 db = SqliteDatabase("hpo.db")
 
+
 class Terms(Model):
-    hpo        = CharField()
-    name       = CharField()
-    definition = CharField(default = None)
-    comment    = CharField(default = None) 
+    hpo = CharField()
+    name = CharField()
+    definition = CharField(default=None)
+    comment = CharField(default=None)
+
     class Meta:
-        database = db 
+        database = db
 
 
 class Nodes(Model):
-    name      = CharField()
-    left      = IntegerField()
-    right     = IntegerField()
-    depth     = IntegerField()
-    parent    = ForeignKeyField('self', null=True)
-    term      = ForeignKeyField(Terms)
+    name = CharField()
+    left = IntegerField()
+    right = IntegerField()
+    depth = IntegerField()
+    parent = ForeignKeyField("self", null=True)
+    term = ForeignKeyField(Terms)
 
     class Meta:
-        database = db 
+        database = db
 
 
 class Informations(Model):
     hpo_version = CharField()
-    hpo_md5     = CharField()
-    saved_by    = CharField()
-    version     = CharField()
+    hpo_md5 = CharField()
+    saved_by = CharField()
+    version = CharField()
 
     class Meta:
-        database = db 
-
+        database = db
 
 
 class Genes(Model):
     entrez_id = IntegerField()
-    name      = CharField()
+    name = CharField()
+
     class Meta:
-        database = db 
+        database = db
 
 
-class Term_has_Genes(Model):
-    term      = ForeignKeyField(Terms)
-    gene      = ForeignKeyField(Genes)
+class Genes_has_Terms(Model):
+    term = ForeignKeyField(Terms)
+    gene = ForeignKeyField(Genes)
+
     class Meta:
-        database = db 
-
+        database = db
 
 
 class Diseases(Model):
-    source    = CharField()
-    source_id = IntegerField()
-    name      = CharField()
-    term      = ForeignKeyField(Nodes)
-    evidence  = CharField()
-
-
+    name = CharField()
+    database_id = CharField()
+    name = CharField()
 
     class Meta:
-        database = db 
+        database = db
+
+
+class Disease_has_Terms(Model):
+    term = ForeignKeyField(Terms)
+    disease = ForeignKeyField(Diseases)
+    qualifier = BooleanField()
+    evidence = CharField()
+    frequency = CharField()
+    aspect = CharField(max_length=1)
 
 
 def create_database_shema():
     db.connect()
-    db.create_tables([Nodes,Terms,Informations])
+    db.create_tables([Nodes, Terms, Informations])
     return db
